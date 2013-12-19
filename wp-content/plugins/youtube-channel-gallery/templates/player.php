5b0e64b686705d3c99c449c363c0e72a95a7ab4d
Amy Greene <amygreene@gmail.com>
2013-12-19 15:41:15 -0800

    update to youtube chanel gallery

--- wp-content/plugins/youtube-channel-gallery/templates/player.php
+++ wp-content/plugins/youtube-channel-gallery/templates/player.php
@@ -1,5 +1,4 @@
 <?php
-	
 
 	// Load js
 	$this->register_scripts();
@@ -12,5 +11,5 @@
 	$content .= '<div class="ytcplayer-wrapper ytc-player' . $ytchag_ratio . '">';
 	
 	//iframe
-	$content .= '<iframe id="ytcplayer' . $plugincount . '" class="ytcplayer" allowfullscreen src="http://www.youtube.com/embed/' . $youtubeid . '?version=3' . $ytchag_theme . $ytchag_color .  $ytchag_autoplay.  $ytchag_modestbranding . $ytchag_rel . $ytchag_showinfo .'&enablejsapi=1&wmode=transparent" frameborder="0"></iframe>';
+	$content .= '<iframe id="ytcplayer' . $plugincount . '" class="ytcplayer" allowfullscreen src="' . $youtube_url . '/embed/' . $youtubeid . '?version=3' . $ytchag_theme . $ytchag_color .  $ytchag_autoplay.  $ytchag_modestbranding . $ytchag_rel . $ytchag_showinfo .'&enablejsapi=1&wmode=transparent" frameborder="0"></iframe>';
 	$content .= '</div></div>';
