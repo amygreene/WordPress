8abdf69aba4672664ec3455b274c9dcb29c60c95
Amy Greene <amygreene@gmail.com>
2014-09-15 16:09:45 -0700

    custom header image and footer

---
+++ wp-content/themes/evergreen-hemingway/functions.php
@@ -0,0 +1,11 @@
+<?php
+function evergreen_hemingway_setup() {
+		
+	// Custom header
+	$args = array(		
+		'default-image' => get_bloginfo('stylesheet_directory') . '/images/header.jpg',		
+	);
+	add_theme_support( 'custom-header', $args );
+}
+add_action( 'after_setup_theme', 'evergreen_hemingway_setup');
+?>
