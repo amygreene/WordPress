a870128d217e8857a4471b9ad7fde987e28d60b8
Amy Greene <amygreene@gmail.com>
2014-09-26 12:25:30 -0700

    fix to force auth on protected downloads

--- wp-content/themes/evergreen-baskerville/functions.php
+++ wp-content/themes/evergreen-baskerville/functions.php
@@ -11,4 +11,18 @@
 	);
 	add_theme_support( 'custom-header', $args );
 	
+	/** Downloads Monitor redirection to login page for non-logged in members trying to download content */
+
+		function redirection() {
+		    $redirected_url = $_SERVER['HTTP_REFERER'];
+		    $download_url = $_SERVER['REQUEST_URI'];
+		    $download_redirect = urlencode("{$redirected_url}?redirect-to={$download_url}");
+
+		    return home_url()."/wp-login.php";
+
+		}
+
+		// filter calls
+
+		add_filter('dlm_access_denied_redirect','redirection');
 ?>
