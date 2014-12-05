07efa23f94dad5564022828ee42d8c99d5d9fecd
Amy Greene <amygreene@gmail.com>
2014-12-05 11:46:33 -0800

    addition to functions.php to enable import of custom taxonomy Vocab_Category using CSV Importer plugin

--- wp-content/themes/evergreen-baskerville-collab-customfields/functions.php
+++ wp-content/themes/evergreen-baskerville-collab-customfields/functions.php
@@ -11,4 +11,19 @@
 	);
 	add_theme_support( 'custom-header', $args );
 	
+
+
+	// Set up custom taxonomies for CSV Importer custom-taxonomies.csv example.
+
+	add_action('init', 'csv_importer_taxonomies', 0);
+
+	function csv_importer_taxonomies() {
+	    register_taxonomy('vocab_category', 'vocabulary', array(
+	        'hierarchical' => true,
+	        'label' => 'Vocab Category',
+	    ));
+	    
+	}
+
+
 ?>
