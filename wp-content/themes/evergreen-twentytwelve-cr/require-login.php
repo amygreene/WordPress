b4bf40e9ef87bdebf248bcc4647f7ca1f9ecfec4
Amy Greene <greenea@evergreen.edu>
2015-04-08 13:37:51 -0700

    updates to comments and removal of test code

--- wp-content/themes/evergreen-twentytwelve-cr/require-login.php
+++ wp-content/themes/evergreen-twentytwelve-cr/require-login.php
@@ -1,12 +1,17 @@
 <?php
 /**
- Template Name: Require Login
+* Template Name: Require Login
+*
+* @package WordPress
+* @subpackage Twenty_Twelve
+* @since Twenty Twelve 1.0
  */
+//require authentication and then redirect back to originating page
 auth_redirect();
+
  //page template begins
 
 get_header(); ?>
-    <p> Hello - this page is protected</p>
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
 
