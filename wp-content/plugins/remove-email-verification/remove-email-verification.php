6fc5c5a095eae047a37391217142287fe5e95aba
Amy Greene <greenea@evergreen.edu>
2015-04-17 14:09:18 -0700

    migrate Remove Email Verification code to main repo

---
+++ wp-content/plugins/remove-email-verification/remove-email-verification.php
@@ -0,0 +1,36 @@
+<?php
+/*
+Plugin Name: Remove Email Verification
+Plugin URI: http://premium.wpmudev.org/project/remove-email-verification-from-signup
+Description: This plugin automatically activates and log in users and blog signups, effectively disabling the need for the user to respond to a verification e-mail.
+Author: Barry (Incsub), Marko Miljus (Incsub)
+Version: 2.1.4
+Author URI:
+WDP ID: 74
+Network: true
+*/
+
+/*
+Copyright 2007-2012 Incsub (http://incsub.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License (Version 2 - GPLv2) as published by
+the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+if( is_admin() ) {
+	include_once('external/wpmudev-dash-notification.php');
+} else {
+	require_once('classes/public.removeverification.php');
+
+	$removeemailverification = new removeemailverification();	 	 	  					
+}
