4f32132f98c74bbcf603a3c89dd191cf6fe9e081
Amy Greene <greenea@evergreen.edu>
2015-04-20 15:02:44 -0700

    update Download Monitor plugin from vs. 1.5.1 to 1.7.1 - this will be the last update before it is removed

--- wp-content/plugins/download-monitor/includes/class-dlm-download-version.php
+++ wp-content/plugins/download-monitor/includes/class-dlm-download-version.php
@@ -1,20 +1,78 @@
 <?php
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} // Exit if accessed directly
+
 /**
  * DLM_Download_Version class.
  */
 class DLM_Download_Version {
 
+	/** @var int */
+	public $id;
+
+	/** @var int */
+	public $download_id;
+
+	/** @var string */
+	public $version;
+
+	/** @var int */
+	public $download_count;
+
+	/** @var int */
+	public $filesize;
+
+	/** @var string */
+	public $md5;
+
+	/** @var string */
+	public $sha1;
+
+	/** @var string */
+	public $crc32;
+
+	/** @var array */
+	public $mirrors;
+
+	/** @var string */
+	public $url;
+
+	/** @var string */
+	public $filename;
+
+	/** @var string */
+	public $filetype;
+
 	/**
 	 * __construct function.
 	 *
+	 * @param int $version_id
+	 * @param int $download_id
+	 *
 	 * @access public
-	 * @return void
 	 */
-	public function __construct( $version_id, $download_id ) {
-		$this->id             = absint( $version_id );
-		$this->download_id    = absint( $download_id );
-		
+	public function __construct( $version_id = 0, $download_id = 0 ) {
+
+		// Check if both version and download id are given in constructor
+		if ( 0 !== $version_id && 0 !== $download_id ) {
+			// Setup the class with DB data
+			$this->setup( $version_id, $download_id );
+		}
+	}
+
+	/**
+	 * Load data from DB. Not the ideal way to do this but we can't break BC.
+	 *
+	 * @param int $version_id
+	 * @param int $download_id
+	 */
+	private function setup( $version_id, $download_id ) {
+
+		$this->id          = absint( $version_id );
+		$this->download_id = absint( $download_id );
+
 		// Get Version Data
 		$this->version        = strtolower( get_post_meta( $this->id, '_version', true ) );
 		$this->download_count = get_post_meta( $this->id, '_download_count', true );
@@ -33,14 +91,24 @@ class DLM_Download_Version {
 			$this->mirrors = array();
 		}
 
-		$this->url            = current( $this->mirrors );
-		$this->filename       = current( explode( '?', basename( $this->url ) ) );
-		$this->filetype       = strtolower( substr( strrchr( $this->filename, "." ), 1 ) );
+		$this->url      = current( $this->mirrors );
+		$this->filename = current( explode( '?', basename( $this->url ) ) );
+		$this->filetype = strtolower( substr( strrchr( $this->filename, "." ), 1 ) );
 
 		// If we don't have a filesize, lets get it now
 		if ( $this->filesize === "" ) {
 			$this->filesize = $this->get_filesize( $this->url );
 		}
+
+	}
+
+	/**
+	 * Get the version slug
+	 *
+	 * @return string
+	 */
+	public function get_version_slug() {
+		return sanitize_title_with_dashes( $this->version );
 	}
 
 	/**
@@ -63,13 +131,17 @@ class DLM_Download_Version {
 	 * get_filesize function.
 	 *
 	 * @access public
-	 * @param mixed $file
+	 *
+	 * @param string $file_path
+	 *
 	 * @return string
 	 */
 	public function get_filesize( $file_path ) {
-		global $download_monitor;
+		// File Manager
+		$file_manager = new DLM_File_Manager();
 
-		$filesize = $download_monitor->get_filesize( $file_path );
+		// Get the file size
+		$filesize = $file_manager->get_file_size( $file_path );
 
 		update_post_meta( $this->id, '_filesize', $filesize );
 
@@ -80,13 +152,18 @@ class DLM_Download_Version {
 	 * get_file_hashes function.
 	 *
 	 * @access public
-	 * @param mixed $file
+	 *
+	 * @param string $file_path
+	 *
 	 * @return array
 	 */
 	public function get_file_hashes( $file_path ) {
-		global $download_monitor;
 
-		$hashes = $download_monitor->get_file_hashes( $file_path );
+		// File Manager
+		$file_manager = new DLM_File_Manager();
+
+		// Get the hashes
+		$hashes = $file_manager->get_file_hashes( $file_path );
 
 		update_post_meta( $this->id, '_md5', $hashes['md5'] );
 		update_post_meta( $this->id, '_sha1', $hashes['sha1'] );
